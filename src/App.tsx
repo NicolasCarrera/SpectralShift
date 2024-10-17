6f6bd4d930686755b3fc6ccf0768b0177086e5d8
NicolasCarrera <nacarrera@uce.edu.ec>
2024-10-17 18:34:29 -0500

    feat(page): main layout of the page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Header } from './components/Header'
 import { ThemeProvider } from './contexts/ThemeContext'
 import 'two-up-element'
-import Gallery from './pages/Gallery'
+import Gallery from './sections/Gallery'
 import Footer from './components/Footer'
 import { useTranslations } from './hooks/useTranslations'
 
